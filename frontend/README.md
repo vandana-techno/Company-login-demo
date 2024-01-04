6ffb8c45fe851a1c9e7a7a76b2da02ee4a337159
Shubhamraj-github <srshubhamraj98841@gmail.com>
2024-01-04 13:28:00 +0530

    Adding frontend template

--- frontend/README.md
+++ frontend/README.md
@@ -1,8 +1,3 @@
-# React + Vite
+# mantis-react-js
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Web theme with name as 'Mantis'. Its name of insect but we derived it from two popular UI framework, Material-UI and Ant Design (M-Ant-is). It provides best features from each. Follows design principle of AntDesign and uses the Component of Material-UI.
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RouterProvider } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+// project import
+import router from 'routes';
+import ThemeCustomization from 'themes';
 
+import Locales from 'components/Locales';
+import RTLLayout from 'components/RTLLayout';
+import ScrollTop from 'components/ScrollTop';
+import Snackbar from 'components/@extended/Snackbar';
+import Notistack from 'components/third-party/Notistack';
+
+// auth provider
+import { JWTProvider as AuthProvider } from 'contexts/JWTContext';
+// import { FirebaseProvider as AuthProvider } from 'contexts/FirebaseContext';
+// import { AWSCognitoProvider as AuthProvider } from 'contexts/AWSCognitoContext';
+// import { Auth0Provider as AuthProvider } from 'contexts/Auth0Context';
+
+// ==============================|| APP - THEME, ROUTER, LOCAL ||============================== //
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev"  target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ThemeCustomization>
+      <RTLLayout>
+        <Locales>
+          <ScrollTop>
+            <AuthProvider>
+              <>
+                <Notistack>
+                  <RouterProvider router={router} />
+                  <Snackbar />
+                </Notistack>
+              </>
+            </AuthProvider>
+          </ScrollTop>
+        </Locales>
+      </RTLLayout>
+    </ThemeCustomization>
+  );
+};
 
-export default App
+export default App;
